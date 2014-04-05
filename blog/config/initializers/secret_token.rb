89c9b8e8204ff6aeba706f19c900585ebd3d436f
Jiawei Lin <jiaweilin02@gmail.com>
2014-04-05 16:53:26 +0800

    Pushing CourseraRubyRails.

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '0f156a55f70ad3f1e617461ba49e5b7edd2e4039345ee3a66c0380fd059166a32594975685c7d2e915a7294cf7cd6750b5ace382c0ad2292eb09362ea8b20f56'
